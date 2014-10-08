2d40231d9c7cc2672535028adc35089314e493e2
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-10-08 16:21:40 +0200

    hwtimer overhead and vtimer replaced

--- cpu/stm32f1/periph/timer.c
+++ cpu/stm32f1/periph/timer.c
@@ -406,4 +406,7 @@ static inline void irq_handler(tim_t timer, TIM_TypeDef *dev0, TIM_TypeDef *dev1
     else {
         dev0->SR = 0;
     }
+    if (sched_context_switch_request) {
+        thread_yield();
+    }
 }

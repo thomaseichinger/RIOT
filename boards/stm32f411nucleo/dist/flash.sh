1a4fb51fcaa91d1aaf6b3051d985aa8ccf68fe4b
Christian Mehlis <mehlis@inf.fu-berlin.de>
2014-11-25 00:24:57 +0100

    added new board stm32f411nucleo

---
+++ boards/stm32f411nucleo/dist/flash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+openocd -f "${RIOTBOARD}/${BOARD}/dist/openocd.cfg" \
+    -c "init" \
+    -c "targets" \
+    -c "reset" \
+    -c "halt" \
+    -c "flash protect 0 0 11 off" \
+    -c "flash erase_address 0x08000000 0x40000" \
+    -c "flash write_image erase $1 0 ihex" \
+    -c "verify_image $1" \
+    -c "reset run"\
+    -c "shutdown"

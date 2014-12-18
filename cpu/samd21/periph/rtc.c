5e53ce539620c55e7eecb723cdfd7d6eafef5cd2
bapclenet <bapclenet@gmail.com>
2014-12-18 11:38:13 +0100

    Styling edits

--- cpu/samd21/periph/rtc.c
+++ cpu/samd21/periph/rtc.c
@@ -32,6 +32,9 @@ typedef struct {
 
 static rtc_state_t rtc_callback;
 
+/* At 1Hz, RTC goes till 63 years (2^5, see 17.8.22 in datasheet)
+* reference_year is set to 100 (offset) to be in our current time (2000)
+* Thanks to this, the user will be able to set time in 2000's*/
 static uint16_t reference_year = 100;
 
 void rtc_init(void)
@@ -48,7 +51,7 @@ void rtc_init(void)
     SYSCTRL->OSC32K.bit.EN32K = 1;
     SYSCTRL->OSC32K.bit.ENABLE = 1;
 
-    /* Setup clock GCLK2 */
+    /* Setup clock GCLK2 with OSC32K divided by 32 */
     GCLK->GENDIV.reg = GCLK_GENDIV_ID(2)|GCLK_GENDIV_DIV(4);
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
     GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSC32K | GCLK_GENCTRL_ID(2) | GCLK_GENCTRL_DIVSEL );
@@ -57,29 +60,29 @@ void rtc_init(void)
     while (GCLK->STATUS.bit.SYNCBUSY);
 
     /* DISABLE RTC MASTER */
-    while(rtcMode2->STATUS.reg & RTC_STATUS_SYNCBUSY);
+    while (rtcMode2->STATUS.reg & RTC_STATUS_SYNCBUSY);
     rtc_poweroff();
 
     /* Reset RTC */
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     rtcMode2->CTRL.reg= RTC_MODE2_CTRL_SWRST;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
 
     /* RTC config with RTC_MODE2_CTRL_CLKREP = 0 (24h) */
     rtcMode2->CTRL.reg = RTC_MODE2_CTRL_PRESCALER_DIV1024|RTC_MODE2_CTRL_MODE_CLOCK;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     rtcMode2->INTENSET.reg = RTC_MODE2_INTENSET_OVF;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     rtc_poweron();
 }
 
 int rtc_set_time(struct tm *time)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
-    if((time->tm_year < reference_year) || (time->tm_year > reference_year + 63)){
+    if ((time->tm_year < reference_year) || (time->tm_year > reference_year + 63)) {
         return -1;
     }
-    else{
+    else {
         rtcMode2->CLOCK.reg = RTC_MODE2_CLOCK_YEAR(time->tm_year - reference_year)
                 | RTC_MODE2_CLOCK_MONTH(time->tm_mon + 1)
                 | RTC_MODE2_CLOCK_DAY(time->tm_mday)
@@ -87,7 +90,7 @@ int rtc_set_time(struct tm *time)
                 | RTC_MODE2_CLOCK_MINUTE(time->tm_min)
                 | RTC_MODE2_CLOCK_SECOND(time->tm_sec);
     }
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     return 0;
 }
 
@@ -95,15 +98,15 @@ int rtc_get_time(struct tm *time)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     time->tm_year = rtcMode2->CLOCK.bit.YEAR + reference_year;
-    if((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))){
-            return -1;
-        }
+    if ((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))) {
+        return -1;
+    }
     time->tm_mon = rtcMode2->CLOCK.bit.MONTH - 1;
     time->tm_mday = rtcMode2->CLOCK.bit.DAY;
     time->tm_hour = rtcMode2->CLOCK.bit.HOUR;
     time->tm_min = rtcMode2->CLOCK.bit.MINUTE;
     time->tm_sec = rtcMode2->CLOCK.bit.SECOND;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     return 0;
 }
 
@@ -111,10 +114,10 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     rtc_clear_alarm();
-    if((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))){
+    if ((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))) {
         return -2;
     }
-    else{
+    else {
         rtcMode2->Mode2Alarm[0].ALARM.reg = RTC_MODE2_ALARM_YEAR(time->tm_year - reference_year)
                 | RTC_MODE2_ALARM_MONTH(time->tm_mon + 1)
                 | RTC_MODE2_ALARM_DAY(time->tm_mday)
@@ -123,7 +126,7 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
                 | RTC_MODE2_ALARM_SECOND(time->tm_sec);
         rtcMode2->Mode2Alarm[0].MASK.reg = RTC_MODE2_MASK_SEL(6);
     }
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
 
     /* Setup interrupt */
     NVIC_SetPriority(RTC_IRQn, 10);
@@ -133,9 +136,9 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
     rtc_callback.cb = cb;
     rtc_callback.arg = arg;
     rtcMode2->INTFLAG.reg = RTC_MODE2_INTFLAG_ALARM0;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
     rtcMode2->INTENSET.reg = RTC_MODE2_INTENSET_ALARM0;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
 
     return 0;
 }
@@ -144,9 +147,9 @@ int rtc_get_alarm(struct tm *time)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     time->tm_year = rtcMode2->Mode2Alarm[0].ALARM.bit.YEAR + reference_year;
-    if((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))){
-            return -1;
-        }
+    if ((time->tm_year < reference_year) || (time->tm_year > (reference_year + 63))) {
+        return -1;
+    }
     time->tm_mon = rtcMode2->CLOCK.bit.MONTH - 1;
     time->tm_mday = rtcMode2->CLOCK.bit.DAY;
     time->tm_hour = rtcMode2->CLOCK.bit.HOUR;
@@ -169,25 +172,25 @@ void rtc_poweron(void)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     rtcMode2->CTRL.bit.ENABLE = 1;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
 }
 
 void rtc_poweroff(void)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     rtcMode2->CTRL.bit.ENABLE = 0;
-    while(rtcMode2->STATUS.bit.SYNCBUSY);
+    while (rtcMode2->STATUS.bit.SYNCBUSY);
 }
 
 void isr_rtc(void)
 {
     RtcMode2 *rtcMode2 = &(RTC_DEV);
     uint16_t status = rtcMode2->INTFLAG.reg;
-    if(status & RTC_MODE2_INTFLAG_ALARM0){
+    if (status & RTC_MODE2_INTFLAG_ALARM0) {
         rtc_callback.cb(rtc_callback.arg);
         rtcMode2->INTFLAG.reg = RTC_MODE2_INTFLAG_ALARM0;
     }
-    if(status & RTC_MODE2_INTFLAG_OVF){
+    if (status & RTC_MODE2_INTFLAG_OVF) {
         /* At 1Hz, RTC goes till 63 years (2^5, see 17.8.22 in datasheet)
         * Start RTC again with reference_year 64 years more (Be careful with alarm set) */
         reference_year += 64;

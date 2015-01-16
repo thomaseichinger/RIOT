ab1ed24eb2ced71204a2da066906786b8107a1f8
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2015-01-16 17:44:17 +0100

    boards/iot-lab_M3: name variable in flash script

--- boards/iot-lab_M3/dist/flash.sh
+++ boards/iot-lab_M3/dist/flash.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+IMAGE=$1
+
 openocd -f "${RIOTBOARD}/${BOARD}/dist/${BOARD}_jtag.cfg" \
     -f "target/stm32f1x.cfg" \
     -c "init" \
     -c "targets" \
     -c "reset halt" \
     -c "reset init" \
-    -c "flash write_image erase $1" \
-    -c "verify_image $1" \
+    -c "flash write_image erase ${IMAGE}" \
+    -c "verify_image ${IMAGE}" \
     -c "reset run"\
     -c "shutdown"

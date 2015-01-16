a86bff0c9dbb1eaa6b88072dfb034bec4ca47f8a
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2015-01-16 17:11:01 +0100

    boards/samr21-xpro: name variable in flash script

--- boards/samr21-xpro/dist/flash.sh
+++ boards/samr21-xpro/dist/flash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IMAGE=${1}
 TARGET=${2}
 
 openocd -f "board/atmel_samr21_xplained_pro.cfg" \
@@ -7,8 +8,8 @@ openocd -f "board/atmel_samr21_xplained_pro.cfg" \
     -c "targets" \
     -c "reset halt" \
     -c "reset init" \
-    -c "flash write_image erase $1" \
-    -c "verify_image $1" \
+    -c "flash write_image erase ${IMAGE}" \
+    -c "verify_image ${IMAGE}" \
     -c "reset run" \
     -c "shutdown" \
     ${TARGET}

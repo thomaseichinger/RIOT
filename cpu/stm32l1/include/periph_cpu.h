fd8d52152c12e8736c7531df23acb751cb8cff52
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-06-14 15:28:30 +0200

    cpu: adapted UART driver to changed interface

--- cpu/stm32l1/include/periph_cpu.h
+++ cpu/stm32l1/include/periph_cpu.h
@@ -18,8 +18,8 @@
  * @author          Katja Kirstein <katja.kirstein@haw-hamburg.de>
  */
 
-#ifndef PERIPH_CPU_H_
-#define PERIPH_CPU_H_
+#ifndef PERIPH_CPU_H
+#define PERIPH_CPU_H
 
 #include "cpu.h"
 
@@ -102,5 +102,5 @@ typedef struct {
 }
 #endif
 
-#endif /* PERIPH_CPU_H_ */
+#endif /* PERIPH_CPU_H */
 /** @} */

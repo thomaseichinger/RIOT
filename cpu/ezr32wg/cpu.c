518d57d0e639f3828f4a4cadbcc7dbaae1d7e211
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-08-06 13:45:50 +0200

    s cpu: added clock initialization

--- cpu/ezr32wg/cpu.c
+++ cpu/ezr32wg/cpu.c
@@ -22,13 +22,35 @@
 
 /**
  * @brief   Configure clock sources and the CPU frequency
+ *
+ * On the ezr32wg cpu, we basically have two options for selecting the main
+ * clock source, using an external clock source (e.g. a crystal), or using the
+ * internal RC oscillator (enabled by default).
  */
 static void clk_init(void)
 {
-    /* TODO */
+    /* reset the clock to it's default values */
+    CMU->CTRL = _CMU_CTRL_RESETVALUE;
 
-    /* enable high frequency peripheral clock with prescale factor 0 */
-    CMU->HFPERCLKDIV = CMU_HFPERCLKDIV_HFPERCLKEN;
+#ifdef CLOCK_HFXO
+    /* configure clock system for external oscillator */
+#if CLOCK_HFXO > 32000000
+    CMU->CTRL |= CMU_CTRL_HFXOBUFCUR_BOOSTABOVE32MHZ;
+#endif
+    /* enable the oscillator */
+    CMU->OSCENCMD = CMU_OSCENCMD_HFXOEN;
+    /* wait for external oscillator to be stable */
+    while (!(CMU->STATUS & CMU_STATUS_HFXORDY));
+    /* switch to the external oscillator */
+    CMU->CMD = CMU_CMD_HFCLKSEL_HFXO;
+    /* wait until clock source is applied */
+    while (!(CMU->STATUS & CMU_STATUS_HFXOSEL));
+#endif
+
+    /* set the core clock divider */
+    CMU->HFCORECLKDIV = (CLOCK_HFCORECLKDIV - 1);
+    /* configure the high speed peripheral clock: set divider and enable it */
+    CMU->HFPERCLKDIV = (CMU_HFPERCLKDIV_HFPERCLKEN | (CLOCK_HFPERCLKDIV - 1));
 }
 
 void cpu_init(void)

a6db698c148b821d4583205f2d6c12a2a27a424f
Thomas Eichinger <thomas.eichinger1@gmail.com>
2016-01-21 11:34:01 +0100

    radiotimer: change to use xtimer

--- sys/net/gnrc/link_layer/6tisch/riot-adaption/radiotimer.c
+++ sys/net/gnrc/link_layer/6tisch/riot-adaption/radiotimer.c
@@ -1,6 +1,6 @@
 #include "stdint.h"
 
-#include "periph/timer.h"
+#include "xtimer.h"
 
 #include "radiotimer.h"
 #include "board_info.h"
@@ -22,27 +22,18 @@ enum  radiotimer_irqstatus_enum{
 typedef struct {
    radiotimer_compare_cbt    overflow_cb;
    radiotimer_compare_cbt    compare_cb;
-   radiotimer_capture_cbt    start_frame_cb;
-   radiotimer_capture_cbt    end_frame_cb;
-   uint8_t                   overflowORcompare;
    uint32_t                  currentSlotPeriod;
+   xtimer_t                  p_timer;
+   xtimer_t                  s_timer;
 } radiotimer_vars_t;
 
-volatile radiotimer_vars_t radiotimer_vars;
+radiotimer_vars_t radiotimer_vars;
 
-/* assumption: timer runs with 1MHz */
-#define TIMER_TICKS(x)       ((x)*1000)
-#define TIMER_TICKS_TO_US(x) ((x)/1000)
-
-//=========================== prototypes ======================================
-extern int timer_set_relative(tim_t, int channel, unsigned int rel_value);
-//=========================== public ==========================================
-
-//===== admin
 
 void radiotimer_init(void) {
    // clear local variables
    memset((void*)&radiotimer_vars,0,sizeof(radiotimer_vars_t));
+   radiotimer_vars.p_timer.callback = &radiotimer_isr;
 }
 
 void radiotimer_setOverflowCb(radiotimer_compare_cbt cb) {
@@ -53,31 +44,27 @@ void radiotimer_setCompareCb(radiotimer_compare_cbt cb) {
    radiotimer_vars.compare_cb     = cb;
 }
 
-void radiotimer_setStartFrameCb(radiotimer_capture_cbt cb) {
-   radiotimer_vars.start_frame_cb = cb;
-}
-
-void radiotimer_setEndFrameCb(radiotimer_capture_cbt cb) {
-   radiotimer_vars.end_frame_cb = cb;
-}
-
 void radiotimer_start(PORT_RADIOTIMER_WIDTH period) {
     DEBUG("%s\n", __PRETTY_FUNCTION__);
-    timer_init(OWSN_TIMER, 1, &radiotimer_isr);
-    timer_set(OWSN_TIMER, 0, ((unsigned int)TIMER_TICKS(period)));
+    // timer_init(OWSN_TIMER, 1, &radiotimer_isr);
+    // timer_set(OWSN_TIMER, 0, ((unsigned int)TIMER_TICKS(period)));
+    
     radiotimer_vars.currentSlotPeriod = period;
+    xtimer_set(&(radiotimer_vars.p_timer), (uint32_t) period);
 }
 
 //===== direct access
 
 PORT_RADIOTIMER_WIDTH radiotimer_getValue(void) {
-    return (PORT_RADIOTIMER_WIDTH)(TIMER_TICKS_TO_US(timer_read(OWSN_TIMER)));
+    return (PORT_RADIOTIMER_WIDTH) xtimer_now();
 }
 
 void radiotimer_setPeriod(PORT_RADIOTIMER_WIDTH period) {
     DEBUG("%s\n", __PRETTY_FUNCTION__);
-    timer_set(OWSN_TIMER, 0, ((unsigned int)TIMER_TICKS(period)));
+    // timer_set(OWSN_TIMER, 0, ((unsigned int)TIMER_TICKS(period)));
+    
     radiotimer_vars.currentSlotPeriod = period;
+    xtimer_set(&(radiotimer_vars.p_timer), (uint32_t) period);
 }
 
 PORT_RADIOTIMER_WIDTH radiotimer_getPeriod(void) {
@@ -88,35 +75,28 @@ PORT_RADIOTIMER_WIDTH radiotimer_getPeriod(void) {
 
 void radiotimer_schedule(PORT_RADIOTIMER_WIDTH offset) {
     DEBUG("%s\n", __PRETTY_FUNCTION__);
-    timer_set(OWSN_TIMER, 1, TIMER_TICKS(offset));
+    // timer_set(OWSN_TIMER, 1, TIMER_TICKS(offset));
+    xtimer_set(&(radiotimer_vars.s_timer), (uint32_t) offset);
 }
 
 void radiotimer_cancel(void) {
     DEBUG("%s\n", __PRETTY_FUNCTION__);
-    timer_set(OWSN_TIMER, 1, 0);
+    // timer_set(OWSN_TIMER, 1, 0);
+    xtimer_remove(&(radiotimer_vars.s_timer));
 }
 
 //===== capture
 
 inline PORT_RADIOTIMER_WIDTH radiotimer_getCapturedTime(void) {
-    return (PORT_RADIOTIMER_WIDTH)(timer_read(OWSN_TIMER));
+    return (PORT_RADIOTIMER_WIDTH)(xtimer_now());
 }
 
 //=========================== private =========================================
 
 //=========================== interrupt handlers ==============================
-void radiotimer_isr(int channel) {
-    if (channel == 0) {
-      DEBUG("%s cmp\n", __PRETTY_FUNCTION__);
-      if (radiotimer_vars.compare_cb!=NULL) {
-          radiotimer_vars.compare_cb();
-      }
-    }
-    else {
-      DEBUG("%s of\n", __PRETTY_FUNCTION__);
-      if (radiotimer_vars.overflow_cb!=NULL) {
-          timer_set(OWSN_TIMER, 1, 0);
-          radiotimer_vars.overflow_cb();
-      }
+void radiotimer_isr(void* arg) {
+    DEBUG("%s cmp\n", __PRETTY_FUNCTION__);
+    if (radiotimer_vars.compare_cb!=NULL) {
+        radiotimer_vars.compare_cb();
     }
 }

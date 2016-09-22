c353eeb2b22afb298949d2e6713ec6a8a0660472
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-09-22 15:04:34 -0700

    fixup! add SEGGER's license, remove GPL

--- sys/rtt_stdio/rtt_stdio.c
+++ sys/rtt_stdio/rtt_stdio.c
@@ -1,14 +1,64 @@
 /*
  * Copyright (C) 2016 Michael Andersen <m.andersen@cs.berkeley.edu>
  *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- *
  * This file was based on SEGGER's reference implementation
  * which can be found here: https://www.segger.com/jlink-rtt.html
- * (c) 2014-2014 SEGGER Microcontroller GmbH & Co. KG
- */
+ * (c) 2014-2016 SEGGER Microcontroller GmbH & Co. KG
+ * This implementation bore the following license notes:
+ **********************************************************************
+ *               SEGGER MICROCONTROLLER GmbH & Co. KG                 *
+ *       Solutions for real time microcontroller applications         *
+ **********************************************************************
+ *                                                                    *
+ *       (c) 2014 - 2016  SEGGER Microcontroller GmbH & Co. KG        *
+ *                                                                    *
+ *       www.segger.com     Support: support@segger.com               *
+ *                                                                    *
+ **********************************************************************
+ *                                                                    *
+ *       SEGGER RTT * Real Time Transfer for embedded targets         *
+ *                                                                    *
+ **********************************************************************
+ *                                                                    *
+ * All rights reserved.                                               *
+ *                                                                    *
+ * SEGGER strongly recommends to not make any changes                 *
+ * to or modify the source code of this software in order to stay     *
+ * compatible with the RTT protocol and J-Link.                       *
+ *                                                                    *
+ * Redistribution and use in source and binary forms, with or         *
+ * without modification, are permitted provided that the following    *
+ * conditions are met:                                                *
+ *                                                                    *
+ * o Redistributions of source code must retain the above copyright   *
+ *   notice, this list of conditions and the following disclaimer.    *
+ *                                                                    *
+ * o Redistributions in binary form must reproduce the above          *
+ *   copyright notice, this list of conditions and the following      *
+ *   disclaimer in the documentation and/or other materials provided  *
+ *   with the distribution.                                           *
+ *                                                                    *
+ * o Neither the name of SEGGER Microcontroller GmbH & Co. KG         *
+ *   nor the names of its contributors may be used to endorse or      *
+ *   promote products derived from this software without specific     *
+ *   prior written permission.                                        *
+ *                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND             *
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,        *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF           *
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           *
+ * DISCLAIMED. IN NO EVENT SHALL SEGGER Microcontroller BE LIABLE FOR *
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR           *
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT  *
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;    *
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF      *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT          *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE  *
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH   *
+ * DAMAGE.                                                            *
+ *                                                                    *
+ **********************************************************************/
+ 
 
 /**
  * @ingroup sys

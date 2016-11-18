a8ec8ef6e83984dd177d19ae415c2e7ece7881a3
Thomas Eichinger <thomas.eichinger1@gmail.com>
2016-11-17 22:09:40 -0800

    added 802154 sec header file

---
+++ sys/include/net/ieee802154_sec.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    net_ieee802154_sec IEEE802.15.4 Security
+ * @ingroup     net_ieee802154
+ * @brief       IEEE802.15.4 Security header definitions and utility functions
+ * @{
+ *
+ * @file
+ * @brief       IEEE 802.14.4 header definitions
+ *
+ * @author      Thomas Eichinger <thomas@riot-os.org>
+ */
+
+#ifndef IEEE802154_SEC_H_
+#define IEEE802154_SEC_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "byteorder.h"
+#include "net/eui64.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IEEE802154_SEC_H_ */
+/** @} */
